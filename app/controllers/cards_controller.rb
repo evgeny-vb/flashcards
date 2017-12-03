1ab78915a4e2504103b744906ddc21f4852c520b
Evgeny Bochkarev <frenzerog@gmail.com>
2017-12-03 19:32:51 +0400

    Add ability to create/edit/delete cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,7 +3,50 @@
 # Controller responsible for showing and creating cards
 #
 class CardsController < ApplicationController
+  before_action :set_card, only: [:edit, :update, :destroy]
+
   def index
-    @cards = Card.all
+    @cards = Card.all.by_review_date
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to cards_path
+    else
+      render 'cards/new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @card.update(card_params)
+      redirect_to cards_path
+    else
+      render 'cards/edit'
+    end
+  end
+
+  def destroy
+    if @card.destroy
+      redirect_to cards_url
+    else
+      redirect_to cards_url, alert: 'Произошла ошибка'
+    end
+  end
+
+  private
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -3,5 +3,20 @@
 # Model responsible for cards
 #
 class Card < ApplicationRecord
-  validates :original_text, :translated_text, presence: true
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate :text_difference
+
+  before_validation :set_review_date, only: :create
+
+  scope :by_review_date, -> { order(review_date: :desc) }
+
+  private
+  def text_difference
+    return unless original_text.casecmp?(translated_text)
+    errors.add(:translated_text, 'Оригинальный и переведенный текст не могут быть одинаковыми')
+  end
+
+  def set_review_date
+    self.review_date = DateTime.now + 3.days
+  end
 end
