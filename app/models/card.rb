0dae5b55140d2a221c38e1c8b12a0c163c8703ca
Evgeny Bochkarev <frenzerog@gmail.com>
2017-12-08 03:43:58 +0400

    Minor refactor regex

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,7 +6,7 @@ class Card < ApplicationRecord
   belongs_to :user
 
   has_attached_file :picture, styles: { original: '360x360>' }
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :picture, content_type: %r{\Aimage\/.*\z}
 
   validates :user_id, :original_text, :translated_text, :review_date, presence: true
   validate :text_difference
