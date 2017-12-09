07915a1347f151f572b9bd6077dfae5150272785
Evgeny Bochkarev <frenzerog@gmail.com>
2017-12-09 18:40:59 +0400

    One line test

--- spec/models/user.rb
+++ spec/models/user.rb
@@ -5,7 +5,6 @@ RSpec.describe User, type: :model do
   let(:pack) { create :pack, user: user }
 
   context '#update_current_pack' do
-    it { expect(user.update_current_pack(pack))
-           .to change(user.current_pack).from(nil).to(pack) }
+    it { expect(user.update_current_pack(pack)).to change(user.current_pack).from(nil).to(pack) }
   end
 end
